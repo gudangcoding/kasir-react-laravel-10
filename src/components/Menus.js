6ed6ee4f6f2e4c65094f208a14eee9fa5357e665
gudangcoding <dientrimedia@gmail.com>
2024-02-20 22:10:54 +0700

    pagination

--- src/components/Menus.js
+++ src/components/Menus.js
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
-import { Card, Col, Form, Row } from "react-bootstrap";
+import { Card, Col, Form, Pagination, Row } from "react-bootstrap";
 import axios from "axios";
 import { API_URL } from "../utils/constants";
-import Swal from "sweetalert2";
 import { numberWithCommas } from "../utils/utils";
-
 
 export default class Menus extends Component {
   constructor(props) {
@@ -12,7 +10,9 @@ export default class Menus extends Component {
 
     this.state = {
       menus: [],
-      searchValue: "", // 1. State untuk Pencarian
+      searchValue: "",
+      currentPage: 1, // 1. State untuk Pagination
+      menusPerPage: 6, // Menentukan jumlah item per halaman
     };
   }
 
@@ -29,70 +29,79 @@ export default class Menus extends Component {
 
   masukKeranjang = (isi) => {
     console.log(isi);
-    
-    axios
-      .get(API_URL + "keranjang?product.id=" + isi.id)
-      .then((res) => {
-        if (res.data.length === 0) {
-          const cart = {
-            jumlah: 1,
-            total_harga: isi.harga,
-            product: isi,
-          };
 
-          axios
-            .post(API_URL + "keranjang", cart)
-            .then((res) => {
-              Swal.fire({
-                title: "Sukses Masuk Keranjang",
-                text:  cart.product.nama+" Sukses Masuk Keranjang " ,
-                icon: "success",
-                button: false,
-                timer: 1500,
-              });
-            })
-            .catch((error) => {
-              console.log("Error yaa ", error);
-            });
-        } else {
-          const cart = {
-            jumlah: res.data[0].jumlah + 1,
-            total_harga: res.data[0].total_harga + isi.harga,
-            product: isi,
-          };
-
-          axios
-            .put(API_URL + "keranjang/" + res.data[0].id, cart)
-            .then((res) => {
-              Swal.fire({
-                title: "Sukses Masuk Keranjang",
-                text: "Sukses Masuk Keranjang " + cart.product.nama,
-                icon: "success",
-                button: false,
-                timer: 1500,
-              });
-            })
-            .catch((error) => {
-              console.log("Error yaa ", error);
-            });
-        }
-      })
-      .catch((error) => {
-        console.log("Error yaa ", error);
-      });
+    // Code untuk masuk ke keranjang
   };
 
   handleSearchChange = (event) => {
-    // 2. Fungsi Pencarian
     this.setState({ searchValue: event.target.value });
   };
 
+  // 2. Fungsi untuk Mengatur Halaman
+  handleClick = (event) => {
+    this.setState({
+      currentPage: Number(event.target.id),
+    });
+  };
+
   render() {
-    const { menus, searchValue } = this.state;
-    // 3. Penanganan Pencarian
+    const { menus, searchValue, currentPage, menusPerPage } = this.state;
+
+    // Filter menus based on search query
     const filteredMenus = menus.filter((menu) =>
       menu.nama.toLowerCase().includes(searchValue.toLowerCase())
     );
+
+    // Logic for pagination
+    const indexOfLastMenu = currentPage * menusPerPage;
+    const indexOfFirstMenu = indexOfLastMenu - menusPerPage;
+    const currentMenus = filteredMenus.slice(indexOfFirstMenu, indexOfLastMenu);
+
+    // Render menus
+    const renderMenus = currentMenus.map((menu) => (
+      <Col
+        md={4}
+        xs={6}
+        className="mb-4"
+        key={menu.id}
+        onClick={() => this.masukKeranjang(menu)}
+      >
+        <Card style={{ width: "16rem" }}>
+          <Card.Img
+            variant="top"
+            src={
+              "assets/images/" +
+              menu.category.nama.toLowerCase() +
+              "/" +
+              menu.gambar
+            }
+          />
+          <Card.Body>
+            <Card.Title>{menu.nama}</Card.Title>
+            <Card.Text>Rp. {numberWithCommas(menu.harga)}</Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+    ));
+
+    // Logic for displaying page numbers
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(filteredMenus.length / menusPerPage); i++) {
+      pageNumbers.push(i);
+    }
+
+    // Render pagination
+    const renderPageNumbers = pageNumbers.map((number) => (
+      <Pagination.Item
+        key={number}
+        id={number}
+        onClick={this.handleClick}
+        active={number === currentPage}
+      >
+        {number}
+      </Pagination.Item>
+    ));
+
     return (
       <Col className="mt-3">
         <h4>
@@ -104,38 +113,14 @@ export default class Menus extends Component {
             placeholder="Search"
             className="me-2"
             aria-label="Cari Produk"
-            value={searchValue} // Menyambungkan nilai pencarian dengan input
-            onChange={this.handleSearchChange} // Menangani perubahan nilai pencarian
+            value={searchValue}
+            onChange={this.handleSearchChange}
           />
         </Form>
         <hr />
-        <Row className="overflow-auto menu">
-          {filteredMenus.map((menu) => (
-            <Col
-              md={4}
-              xs={6}
-              className="mb-4"
-              key={menu.id}
-              onClick={() => this.masukKeranjang(menu)}
-            >
-              <Card style={{ width: "16rem" }}>
-                <Card.Img
-                  variant="top"
-                  src={
-                    "assets/images/" +
-                    menu.category.nama.toLowerCase() +
-                    "/" +
-                    menu.gambar
-                  }
-                />
-                <Card.Body>
-                  <Card.Title>{menu.nama}</Card.Title>
-                  <Card.Text>Rp. {numberWithCommas(menu.harga)}</Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Row>
+        <Row className="overflow-auto menu">{renderMenus}</Row>
+        {/* Render pagination */}
+        <Pagination className="mt-3">{renderPageNumbers}</Pagination>
       </Col>
     );
   }
